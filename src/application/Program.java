e1ac1e74da062ed4b9049ced82ea0e1781657b26
Rafael Filho <soft_rafael@hotmail.com>
2022-04-30 12:51:38 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Department dep = new Department(1, "Livros");
+		System.out.println(dep);
 	}
 
 }
