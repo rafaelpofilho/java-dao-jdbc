3cd348a15895adc5f938201fd13236a930187e0b
Rafael Filho <soft_rafael@hotmail.com>
2022-04-30 14:16:17 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 				
-		System.out.println(dep);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
-
 }
