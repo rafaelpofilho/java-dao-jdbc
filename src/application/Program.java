f999d131c515d541e4cdfbb15e35d34131ec4e7f
Rafael Filho <soft_rafael@hotmail.com>
2022-05-01 15:42:59 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-				
+						
 		System.out.println("=== TEST 1 - findById ===");
 		Seller seller = sellerDao.findById(3);
 		
@@ -21,11 +21,15 @@ public class Program {
 		
 		System.out.println("=== TEST 2 - findByDepartment ===");
 		Department department = new Department(2, null);
+		
 		List<Seller> list = sellerDao.findByDepartment(department);
 		
 		for (Seller x : list) {
 			System.out.println(x);
 		}
+		
+		// Mesma função do for acima, porém, com 1 linha.
+		list.forEach(System.out::println);
 		
 		System.out.println("=== TEST 3 - findByDepartment ===");
 		list = sellerDao.findAll();
@@ -34,9 +38,19 @@ public class Program {
 			System.out.println(x);
 		}
 		
+		// Mesma função do for acima, porém, com 1 linha.
+		list.forEach(System.out::println);
+/*		
 		System.out.println("=== TEST 4 - seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+*/		
+		System.out.println("=== TEST 5 - update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Bob Wayne");
+		
+		sellerDao.update(seller);		
+		System.out.println("Update completed!");
 	}
 }
